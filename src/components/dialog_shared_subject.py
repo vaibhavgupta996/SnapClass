f90d22d88165714267f95746e457f660abe723c8
Vaibhav Gupta <vaibhavgupt.455@gmail.com>
2026-08-21 01:26:39 +0530

    Fix app_domain

--- src/components/dialog_shared_subject.py
+++ src/components/dialog_shared_subject.py
@@ -4,7 +4,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain="snapclass-main.streamlit.app"
+    app_domain="https://snapclass-pro.streamlit.app"
     join_url=f"{app_domain}/?join-code={subject_code}"
     
     st.header("Scan To Join")
